00b17423bbbb985f1b59141291fb2b12ddd3b0ec
Brad Nielsen <brad_nielsen29@hotmail.com>
2024-02-01 10:41:58 +1000

    commit

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+alert("I AM ALIVE!!")
+message = "Hello World";
+lucky_number = 7;
+console.log(message + " your lucky number is: " + String(lucky_number));
+for (i=0; i<lucky_number; i++) 
+{ // a block of code in c type languages needs a curly braces.
+    console.log(i); 
+    if (i == 3) // True or False
+    {
+        console.log("3 is awesome");
+    }
+}
